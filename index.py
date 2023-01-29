17d3ccd3107ffcfd49e385e8f7549b04e97bf18f
MrMateo899 <mrmateo899contacto@gmail.com>
2023-01-28 21:31:01 -0500

    UN CAMBIO PEQUEÑO

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 bas = int(input('Inserte una base: '))
 exp = int(input('Inserte un exponente: '))
 
-if exp > 3:
-    print(f'{bas} elevado a {exp} es igual a:', bas**exp)
-elif exp == 2:
+if exp == 2:
     print(f'{bas} al cuadrado es igual a:', bas**2)
 elif exp == 3:
     print(f'{bas} al cubo es igual a:', bas**3)
+else :
+    print(f'{bas} elevado a {exp} es igual a:', bas**exp)
